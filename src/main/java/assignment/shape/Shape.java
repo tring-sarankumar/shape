547422f06271b7812ae9c2986d57f72fe2067649
tringapps-sarankumar <123533909+tringapps-sarankumar@users.noreply.github.com>
2023-01-30 16:53:41 +0530

    commit

--- src/main/java/assignment/shape/Shape.java
+++ src/main/java/assignment/shape/Shape.java
@@ -17,8 +17,8 @@ public class Shape {
                     log.info("Enter The Length 3:");
                     Float trilength3 = sc.nextFloat();
                     ShapeLogic obj = new ShapeLogic("triangle", trilength1, trilength2, trilength3);
-                    log.info("The Perimeter of Triangle is " + obj.perimeter());
-                    log.info("The Area of Triangle is " + obj.area());
+                    obj.perimeter();
+                    obj.area();
 
                 } else if (shapetype == 2) {
                     log.info("Enter The Length:");
@@ -26,15 +26,15 @@ public class Shape {
                     log.info("Enter The width:");
                     Float rectanglewidth = sc.nextFloat();
                     ShapeLogic obj = new ShapeLogic("rectangle", rectanglelength, rectanglewidth);
-                    log.info("The Perimeter of Rectangle is " + obj.perimeter());
-                    log.info("The Area of Rectangle is " + obj.area());
+                    obj.perimeter();
+                    obj.area();
 
                 } else if (shapetype == 3) {
                     log.info("Enter The Radius:");
                     Float circleradius = sc.nextFloat();
                     ShapeLogic obj = new ShapeLogic("circle", circleradius);
-                    log.info("The Perimeter of Circle is " + obj.perimeter());
-                    log.info("The Area of Circle is " + obj.area());
+                    obj.perimeter();
+                    obj.area();
 
                 } else if (shapetype == 4) {
                     break;
