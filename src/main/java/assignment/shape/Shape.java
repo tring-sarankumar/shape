09ce4f7b12d514db87b9bb8658867347d45ae119
tringapps-sarankumar <123533909+tringapps-sarankumar@users.noreply.github.com>
2023-01-31 14:17:29 +0530

    commit

--- src/main/java/assignment/shape/Shape.java
+++ src/main/java/assignment/shape/Shape.java
@@ -5,45 +5,39 @@ public class Shape {
     public static void main(String[] args) {
         Logger log = Logger.getLogger("Shape");
         Scanner sc = new Scanner(System.in);
-        try {
-            while (true) {
-                log.info("1.Triangle,2.rectangle, 3.circle, 4.Cancel . Enter 1/2/3/4");
-                int shapetype = sc.nextInt();
-                if (shapetype == 1) {
-                    log.info("Enter The Length 1:");
-                    Float trilength1 = sc.nextFloat();
-                    log.info("Enter The Length 2:");
-                    Float trilength2 = sc.nextFloat();
-                    log.info("Enter The Length 3:");
-                    Float trilength3 = sc.nextFloat();
-                    ShapeLogic obj = new ShapeLogic("triangle", trilength1, trilength2, trilength3);
-                    obj.perimeter();
-                    obj.area();
+        while (true) {
+            log.info("1.Triangle,2.rectangle, 3.circle, 4.Cancel . Enter 1/2/3/4");
+            int shapetype = sc.nextInt();
+            if (shapetype == 1) {
+                log.info("Enter The Length 1:");
+                Float trilength1 = sc.nextFloat();
+                log.info("Enter The Length 2:");
+                Float trilength2 = sc.nextFloat();
+                log.info("Enter The Length 3:");
+                Float trilength3 = sc.nextFloat();
+                ShapeLogic obj = new ShapeLogic("triangle", trilength1, trilength2, trilength3);
+                obj.perimeter();
+                obj.area();
 
-                } else if (shapetype == 2) {
-                    log.info("Enter The Length:");
-                    Float rectanglelength = sc.nextFloat();
-                    log.info("Enter The width:");
-                    Float rectanglewidth = sc.nextFloat();
-                    ShapeLogic obj = new ShapeLogic("rectangle", rectanglelength, rectanglewidth);
-                    obj.perimeter();
-                    obj.area();
+            } else if (shapetype == 2) {
+                log.info("Enter The Length:");
+                Float rectanglelength = sc.nextFloat();
+                log.info("Enter The width:");
+                Float rectanglewidth = sc.nextFloat();
+                ShapeLogic obj = new ShapeLogic("rectangle", rectanglelength, rectanglewidth);
+                obj.perimeter();
+                obj.area();
 
-                } else if (shapetype == 3) {
-                    log.info("Enter The Radius:");
-                    Float circleradius = sc.nextFloat();
-                    ShapeLogic obj = new ShapeLogic("circle", circleradius);
-                    obj.perimeter();
-                    obj.area();
+            } else if (shapetype == 3) {
+                log.info("Enter The Radius:");
+                Float circleradius = sc.nextFloat();
+                ShapeLogic obj = new ShapeLogic("circle", circleradius);
+                obj.perimeter();
+                obj.area();
 
-                } else if (shapetype == 4) {
-                    break;
-                }
+            } else if (shapetype == 4) {
+                break;
             }
-        } catch (Exception e) {
-            log.info("Exception:" + e);
-        } finally {
-            sc.close();
         }
 
     }
